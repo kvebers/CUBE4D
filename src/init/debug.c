a66d4b6f16df48c632c03898f59f1a224206638e
kvebers <kvebers@student.42heilbronn.de>
2023-05-29 17:49:13 +0200

    Progress save

--- src/init/debug.c
+++ src/init/debug.c
@@ -6,7 +6,7 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 16:11:24 by kvebers           #+#    #+#             */
-/*   Updated: 2023/05/29 16:31:09 by kvebers          ###   ########.fr       */
+/*   Updated: 2023/05/29 16:46:25 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ void	debug_ray(t_ray *ray)
 		printf("==================================");
 		printf("x and camera: %i %f\n",ray->x, ray->camera_x);
 		printf("Plane: %f %f\n", ray->c_p2d.pos_x, ray->c_p2d.pos_x);
-		printf("Delta_dist: %f %f\n", ray->delta_dist.pos_x, ray->delta_dist.pos_y);
+		printf("Delta_dist: %f %f\n", ray->delta_dist.pos_x,
+			ray->delta_dist.pos_y);
 		printf("Directions: %f %f\n", ray->dir.pos_x, ray->dir.pos_x);
-		printf("Wall distance: %f Wall Len: %d\n", ray->p_wall, ray->line_height);
+		printf("Wall distance: %f Wall Len: %d\n",
+			ray->p_wall, ray->line_height);
 	}
 }

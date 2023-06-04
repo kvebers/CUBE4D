3af1538b8915a4bdf358ac93086308358fdd3385
kvebers <kvebers@student.42heilbronn.de>
2023-06-04 07:14:38 +0200

    Changes

--- bonus/init/rendermap_b.c
+++ bonus/init/rendermap_b.c
@@ -6,7 +6,7 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 14:34:49 by kvebers           #+#    #+#             */
-/*   Updated: 2023/06/03 19:11:23 by kvebers          ###   ########.fr       */
+/*   Updated: 2023/06/04 07:06:47 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,8 @@ void	calculate_distance(t_params *params, t_ray *ray)
 	{
 		ray->ray_pos.pos_x += ray->ray_cos;
 		ray->ray_pos.pos_y += ray->ray_sin;
-		x = (int)ray->ray_pos.pos_x;
-		y = (int)ray->ray_pos.pos_y;
+		x = ray->ray_pos.pos_x;
+		y = ray->ray_pos.pos_y;
 		if (params->map->map[x / 64][y / 64] == '1')
 			ray->wall = 1;
 		if (params->map->map[x / 64][y / 64] == '2')
